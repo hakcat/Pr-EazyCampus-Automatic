0cd38e267ff631f2670b931e648232314ef98a8e
Aravindvenma <35398351+hakcat@users.noreply.github.com>
2018-12-04 10:56:16 +0530

    Add files via upload

---
+++ Eazy Campus Automatic.js
@@ -0,0 +1,35 @@
+//TKMCE Eazy Campus Automatic Staff Evaluation Excellent Marker
+//Login to Eazy Campus, Click Staff Evaluation link and click any teacher name
+// Open inspect element in your browser by pressing ctrl+Shift+(I or J)
+// Click console
+//Put this code and hit enter
+
+	radio1=document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl02_rbtn_Choice_1_0')
+	radio1.click()
+
+             radio1= document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl03_rbtn_Choice_2_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl04_rbtn_Choice_3_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl05_rbtn_Choice_4_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl06_rbtn_Choice_5_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl07_rbtn_Choice_6_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl08_rbtn_Choice_7_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl09_rbtn_Choice_8_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl10_rbtn_Choice_9_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl11_rbtn_Choice_10_0')
+             radio1.click()
+             radio1= 	document.getElementById('ctl00_ContentPlaceHolder1_gvQuestionsList_ctl12_rbtn_Choice_11_0')
+             radio1.click()
+             btnsave=   document.getElementById('ctl00_ContentPlaceHolder1_btnSave')
+             btnsave.click()
+
+// Change the element ctl00_ContentPlaceHolder1_gvQuestionsList_ctl12_rbtn_Choice_11_0 if you want to customize
+//Row elements start with ct102 and column starts with 0(0=Excellent 1=Good and so on)
+// Although boring, please use the staff evaluation option to say something about the teacher, if you cannot face him/her directly.
